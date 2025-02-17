afe8e19702d1f169d421c9ad83d87ca895165cec
PUSHKAR SINGH GURJAR <pushkargurjar1234@gmail.com>
2025-02-18 02:31:09 +0530

    basic fully completed

--- script.js
+++ script.js
@@ -1,79 +1,10 @@
-let cv=document.querySelector(".cv-button")
-
-
+const cv=document.querySelector(".cv-button")
 cv.addEventListener("click",()=>{
     const link = document.createElement('a');
     link.href = 'data/fakeResume.pdf'; // Make sure this matches your file path
     link.download = 'PushkarGurjar_CV.pdf'; // Rename during download if needed
     link.click();
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
